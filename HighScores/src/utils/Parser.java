4be52b738fc49f71347da7fbb02a282940b14ad4
JohanBrunet <brunet.johan.23@gmail.com>
2016-04-20 10:24:08 +0200

    Remove printing of some lines

--- HighScores/src/utils/Parser.java
+++ HighScores/src/utils/Parser.java
@@ -5,10 +5,6 @@ public class Parser {
 	public static int parseScore(String s) {
 		String[] parse = s.split(",");
 		int i;
-		for (i = 0; i < parse.length; i++) {
-			System.out.println("Parsed : " + parse[i]);
-		}
-		System.out.println("Score : " + parse[2]);
 		return Integer.parseInt(parse[2]);
 	}
 }
