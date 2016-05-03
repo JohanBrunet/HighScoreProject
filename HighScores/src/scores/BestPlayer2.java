d56cfa5b81cef90b34145334556bdca44daff0ef
JohanBrunet <brunet.johan.23@gmail.com>
2016-05-03 18:02:18 +0200

    Update BestPlayer2
    There is no longer errors in this class

--- HighScores/src/scores/BestPlayer2.java
+++ HighScores/src/scores/BestPlayer2.java
@@ -1,20 +1,37 @@
 package scores;
 
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.Scanner;
-
-import utils.Parser;
-
 public class BestPlayer2 {
-	String player;
-	int score;
-	public int compareTo(BestPlayer p){
-		if (this.score>p.score)
+	private String playerName;
+	private int score;
+	
+	
+	public int compareTo(BestPlayer2 p){
+		if (this.score > p.getScore()) {
 			return 1;
-		else if (this.score==p.score)
-				 return 0;
-			 else return -1;
+		}
+		else if (this.score == p.getScore()) {
+			return 0;
+		}
+		else {
+			return -1;
+		}
 	}
+
+
+	public String getPlayer() {
+		return playerName;
+	}
+
+
+	public void setPlayer(String player) {
+		this.playerName = player;
+	}
+	
+	public int getScore() {
+		return this.score;
+	}
+	
+	public void setScore(int score) {
+		this.score = score;
 	}
 }
